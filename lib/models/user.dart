3034bf7f60f2f0a91d300b1c7ba04a42f43cd89b
Morymirco <morykoulibaly996@gmail.com>
2024-04-17 17:15:23 +0000

    user.daart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,14 +1,36 @@
+import "package:http/http.dart" as http;
+
 class User {
   int id;
-  String first_name;
-  String last_name;
+  String firstName;
+  String lastName;
   String email;
   String image;
 
   User(
-      {required this.id,
-      required this.first_name,
-      required this.last_name,
-      required this.email,
+      {required this.email,
+      required this.id,
+      required this.firstName,
+      required this.lastName,
       required this.image});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        id: json['id'],
+        email: json['email'],
+        firstName: json['first_name'],
+        image: json['avatar'],
+        lastName: json['last_name']);
+  }
+}
+
+Future<List<User>> getUsers() async {
+  List<User> users = [];
+
+  http.Response response =
+      await http.get(Uri.parse("https://reqres.in/api/users?page=1"));
+
+  if (response.statusCode == 200) {}
+
+  return users;
 }
